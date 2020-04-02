05d86460c6e389f78e3b8df3d3d510a0b84e664c
Austen Bright <pureaustenbright@gmail.com>
2020-04-02 02:21:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         end
         if !new_hash[name][data_type]
           new_hash[name][data_type] = []
-        end 
+        end
         binding.pry
       end
     end
