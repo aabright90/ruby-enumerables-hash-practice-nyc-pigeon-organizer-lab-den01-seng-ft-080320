79c979c60720af93a97b2332b2174b80fd2638f3
Austen Bright <pureaustenbright@gmail.com>
2020-04-02 01:31:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   }
   data.each do |data_type, data_value|
     data_value.each do |color, names|
-      names.each do |name| 
+      names.each do |name|
       binding.pry
       end 
     end
